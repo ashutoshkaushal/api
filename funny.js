38243d2bc8e444ff3b933adb1d99e57a1ad55f5d
ashutoshkaushal <aasu.406@gmail.com>
2019-11-26 23:17:00 +0530

    New folder

---
+++ funny.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('button').click(function(){
+    const name = $('#nameid').val();
+    //alert(name);
+    $('.imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}`);
+    const b = $('.imgchange');
+    console.log(b);
+    });
+    });
